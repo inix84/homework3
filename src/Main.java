0ed7aeab2b52c305dc0f446d651b56fd3ac66752
inix84 <inix84@mail.ru>
2022-09-02 23:04:28 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x1=1_000_000_000;
+        byte x2=127;
+        short x3=1_000;
+        long x4=1_000_000_000_000_000_000L;
+        float x5=7.7777777f;
+        double x6=0.1616161616161616;
+
+        System.out.println("Значение переменной x1 с типом int равно "+x1);
+        System.out.println("Значение переменной x2 с типом byte равно "+x2);
+        System.out.println("Значение переменной x3 с типом short равно "+x3);
+        System.out.println("Значение переменной x4 с типом long равно "+x4);
+        System.out.println("Значение переменной x5 с типом float равно "+x5);
+        System.out.println("Значение переменной x6 с типом double равно "+x6);
     }
 }
